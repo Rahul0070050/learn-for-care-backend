a9e97b2fbbdaba18e2f7fb144053f84a579aac26
rahul <rahulor2000@gmail.com>
2023-09-15 14:36:30 +0530

    nodemon conf file added

--- src/db/mysql/index.ts
+++ src/db/mysql/index.ts
@@ -17,7 +17,7 @@ export function mySqlConnect(done: CallableFunction) {
     if (err) return done(err);
     
     const userTable = `
-      CREATE TABLE users (
+      CREATE TABLE IF NOT EXISTS users (
       id INT AUTO_INCREMENT PRIMARY KEY,
       username VARCHAR(50) NOT NULL,
       email VARCHAR(50) NOT NULL UNIQUE,
