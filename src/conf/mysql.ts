f90bf8f59ccf82aeec495340c82b51761f784e4f
rahul <rahulor2000@gmail.com>
2023-09-26 16:08:30 +0530

    mysql config file updated

--- src/conf/mysql.ts
+++ src/conf/mysql.ts
@@ -16,29 +16,29 @@ export function mySqlConnect(done: CallableFunction) {
   db.getConnection((err) => {
     if (err) return done(err);
 
-    //     const userTable = `
-    //       CREATE TABLE IF NOT EXISTS users (
-    //       id INT AUTO_INCREMENT PRIMARY KEY,
-    //       username VARCHAR(50) NOT NULL,
-    //       email VARCHAR(50) NOT NULL UNIQUE,
-    //       type VARCHAR(50) NOT NULL,
-    //       password VARCHAR(500) NOT NULL,
-    //       country VARCHAR(50) NOT NULL,
-    //       city VARCHAR(50) NOT NULL,
-    //       profile_image VARCHAR(300) DEFAULT NULL,
-    //       activate BOOLEAN DEFAULT FALSE,
-    //       block BOOLEAN DEFAULT FALSE,
-    //       joined DATETIME DEFAULT NOW(),
-    //       otp INT,
-    //       certificates INT,
-    //       courses_id INT,
-    //       cart INT
-    //   );`;
+      const userTable = `
+          CREATE TABLE IF NOT EXISTS users (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          username VARCHAR(50) NOT NULL,
+          email VARCHAR(50) NOT NULL UNIQUE,
+          type VARCHAR(50) NOT NULL,
+          password VARCHAR(500) NOT NULL,
+          country VARCHAR(50) NOT NULL,
+          city VARCHAR(50) NOT NULL,
+          profile_image VARCHAR(300) DEFAULT NULL,
+          activate BOOLEAN DEFAULT FALSE,
+          block BOOLEAN DEFAULT FALSE,
+          joined DATETIME DEFAULT NOW(),
+          otp INT,
+          certificates INT,
+          courses_id INT,
+          cart INT
+      );`;
 
-    //     db.query(userTable, (err, result) => {
-    //       if (err) console.log(err);
-    //       else console.log('user table created');
-    //     });
+        db.query(userTable, (err, result) => {
+          if (err) console.log(err);
+          else console.log('user table created');
+        });
 
     //     const unexpectedError = `
     //       CREATE TABLE IF NOT EXISTS errors (
@@ -54,20 +54,20 @@ export function mySqlConnect(done: CallableFunction) {
     //       else console.log('errors table created');
     //     });
 
-    //     const createAdminTable = `
-    //     CREATE TABLE IF NOT EXISTS admin (
-    //     email VARCHAR(50) NOT NULL,
-    //     password VARCHAR(100) NOT NULL,
-    //     otp INT(6) DEFAULT NULL
-    //     activate BOOLEAN DEFAULT FALSE
-    // );`;
+    const createAdminTable = `
+        CREATE TABLE IF NOT EXISTS admin (
+        email VARCHAR(50) NOT NULL,
+        password VARCHAR(300) NOT NULL,
+        otp INT(6) DEFAULT NULL
+        activate BOOLEAN DEFAULT FALSE
+    );`;
 
-    //   db.query(createAdminTable, (err, result) => {
-    //     if (err) console.log(err);
-    //     else console.log('admin table created');
-    //   })
+      db.query(createAdminTable, (err, result) => {
+        if (err) console.log(err);
+        else console.log('admin table created');
+      })
 
-    const createCategoryTable = `
+  const createCategoryTable = `
     CREATE TABLE IF NOT EXISTS category (
     id INT AUTO_INCREMENT PRIMARY KEY,
     category VARCHAR(50) UNIQUE NOT NULL,
