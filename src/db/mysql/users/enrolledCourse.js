3be787916e97f7dc323afef580bcf75d9095738d
rahul <rahulor2000@gmail.com>
2023-11-08 22:19:12 +0530

    course route updated

--- src/db/mysql/users/enrolledCourse.js
+++ src/db/mysql/users/enrolledCourse.js
@@ -9,7 +9,7 @@ export function addCourseToEnrolledCourse(
   return new Promise((resolve, reject) => {
     try {
       let insertQuery = `
-            INSERT INTO enrolled_course (user_id, course_id, progress, validity, color, user_type) VALUES (?,?,?,?,?,?);
+            INSERT INTO enrolled_course (user_id, course_id, progress, validity, color, user_type) VALUES (?,?,?,?,?,?); 
             SELECT LAST_INSERT_ID();
           `;
 
