f867c48b6b556163a92a0c333d425d34038ecd2c
rahul <rahulor2000@gmail.com>
2023-11-09 00:11:13 +0530

    ongoing course added

--- src/db/mysql/users/onGoingCourse.js
+++ src/db/mysql/users/onGoingCourse.js
@@ -4,8 +4,11 @@ export function getOnGoingCourseByIdFromDb(id) {
   return new Promise((resolve, reject) => {
     try {
       let getOnGoingCourseByIdQuery =
-        "SELECT * FROM enrolled_course WHERE id = ?;";
+        `SELECT * FROM enrolled_course 
+        INNER JOIN course ON course.id = enrolled_course.course_id
+        WHERE enrolled_course.id = ?;`;
       db.query(getOnGoingCourseByIdQuery, [id], (err, result) => {
+        console.log(err);
         if (err) return reject(err?.message);
         else return resolve(result);
       });
