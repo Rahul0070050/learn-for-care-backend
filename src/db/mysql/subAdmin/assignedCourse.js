9cba1490db682eac30615aac001d812189823b22
rahul <rahulor2000@gmail.com>
2023-11-12 18:05:50 +0530

    db query uupdated

--- src/db/mysql/subAdmin/assignedCourse.js
+++ src/db/mysql/subAdmin/assignedCourse.js
@@ -4,7 +4,7 @@ export function getAllAssignedCourses(id) {
   return new Promise((resolve, reject) => {
     try {
       let getQuery = `
-      SELECT assigned_course.id AS assignedCourseId, *
+      SELECT assigned_course.id AS assigned_course_id, assigned_course.*, course.*  
       FROM assigned_course
       INNER JOIN course ON assigned_course.course_id = course.id
       WHERE assigned_course.sub_user_id = ?;`;
