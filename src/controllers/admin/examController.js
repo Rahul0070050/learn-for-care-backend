3c617ef560317d9eea14259b9e9d5f7e80ba125d
rahul <rahulor2000@gmail.com>
2023-11-15 21:23:27 +0530

    exam route updated

--- src/controllers/admin/examController.js
+++ src/controllers/admin/examController.js
@@ -10,6 +10,7 @@ import {
 export const examController = {
   createExam: (req, res) => {
     try {
+      console.log(JSON.parse(req.body));
       checkAddExamReqBody(req.body)
         .then((result) => {
           insertQuestionsToExam(result)
