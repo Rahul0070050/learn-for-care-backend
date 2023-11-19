a91526d29ce0b5b19c329a18484b7e8fedd76098
rahul <rahulor2000@gmail.com>
2023-11-19 16:54:26 +0530

    manager route updated

--- src/helpers/jwt.js
+++ src/helpers/jwt.js
@@ -174,13 +174,13 @@ export function createTokenForManager(userData) {
       userData.password = "";
       jwt.sign(
         { ...userData },
-        process.env.JWT_RF_KEY_FOR_MANAGER || "",
+        process.env.JWT_RF_KEY_FOR_USER || "",
         { expiresIn: "365d" },
         (err, reFreshToken) => {
           if (err) return reject(err?.message);
           jwt.sign(
             { ...userData },
-            process.env.JWT_ACC_KEY_FOR_MANAGER || "",
+            process.env.JWT_ACC_KEY_FOR_USER || "",
             { expiresIn: "15d" },
             (err, accessToken) => {
               if (err) return reject(err?.message);
