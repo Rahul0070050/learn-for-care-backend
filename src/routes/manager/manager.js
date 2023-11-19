cf83633ba5f2103557c2fa26832dad6452f18632
rahul <rahulor2000@gmail.com>
2023-11-19 16:46:18 +0530

    manager route updated

--- src/routes/manager/manager.js
+++ src/routes/manager/manager.js
@@ -1,20 +1,20 @@
-import { Router } from "express";
+// import { Router } from "express";
 
-import { validateUser } from "../../middlewares/userAuth.js";
-import { userController } from "../../controllers/user/userController.js";
-import { validateCompanyUserUser } from "../../middlewares/compayUserAuth.js";
+// import { validateUser } from "../../middlewares/userAuth.js";
+// import { userController } from "../../controllers/user/userController.js";
+// import { validateCompanyUserUser } from "../../middlewares/compayUserAuth.js";
 
-const route = Router();
+// const route = Router();
 
-route.get("/data", validateUser, userController.getUserData);
-route.post("/update-user-data", validateUser, userController.updateUserData);
+// route.get("/data", validateUser, userController.getUserData);
+// route.post("/update-user-data", validateUser, userController.updateUserData);
 
-route.post("/create-manager-individual", validateCompanyUserUser, userController.createSubUser);
-route.get("/get-all-sub-users", validateCompanyUserUser, userController.getSubUser);
-route.post("/assign-course-to-sub-user", validateCompanyUserUser, userController.assignCourseToSubUser);
-route.post("/block-sub-user", validateCompanyUserUser, userController.blockSubUser);
-route.post("/unblock-sub-user", validateCompanyUserUser, userController.unBlockSubUser);
-route.get("/get-all-blocked-sub-user", validateCompanyUserUser, userController.getBlocked);
-route.get("/get-all-assigned-course-progress", validateCompanyUserUser, userController.getAllAssignedCourseProgress);
+// route.post("/create-manager-individual", validateCompanyUserUser, userController.createSubUser);
+// route.get("/get-all-sub-users", validateCompanyUserUser, userController.getSubUser);
+// route.post("/assign-course-to-sub-user", validateCompanyUserUser, userController.assignCourseToSubUser);
+// route.post("/block-sub-user", validateCompanyUserUser, userController.blockSubUser);
+// route.post("/unblock-sub-user", validateCompanyUserUser, userController.unBlockSubUser);
+// route.get("/get-all-blocked-sub-user", validateCompanyUserUser, userController.getBlocked);
+// route.get("/get-all-assigned-course-progress", validateCompanyUserUser, userController.getAllAssignedCourseProgress);
 
-export default route;
+// export default route;
