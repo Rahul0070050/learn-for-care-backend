b06c9176dd07ce62cc2e87e316afaacd5da55404
rahul <rahulor2000@gmail.com>
2023-11-22 20:07:15 +0530

    get bundles route updated

--- src/db/mysql/users/purchasedCourse.js
+++ src/db/mysql/users/purchasedCourse.js
@@ -56,7 +56,10 @@ export function getPurchasedCourseFromDbByUserId(id) {
   return new Promise((resolve, reject) => { 
     try {
       let getQuery = `
-          SELECT * FROM purchased_course WHERE course_type = ? AND user_id = ?;
+          SELECT purchased_course.* , course.name AS course_name 
+          FROM purchased_course
+          INNER JOIN course_bundle ON purchased_course.course_id = course_bundle.id
+          WHERE purchased_course.course_type = ? AND purchased_course.user_id = ?;
         `
       db.query(getQuery,['bundle',id],(err,result) => {
         if(err){
