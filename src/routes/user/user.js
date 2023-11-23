e0058bd51979f44882aceb3fd11bbedd950ae79a
rahul <rahulor2000@gmail.com>
2023-11-23 14:28:33 +0530

    get manager individial route updated

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -21,7 +21,7 @@ route.get("/get-all-blocked-sub-user", validateCompanyUserUser, userController.g
 route.get("/get-all-assigned-course-progress", validateCompanyUserUser, userController.getAllAssignedCourseProgress);
 
 // manager-individual
-route.post("/create-manager-individual", validateManagerUser, userController.createManagerIndividual);
+route.post("/create-manager-individual", validateCompanyOrManagerUser, userController.createManagerIndividual);
 route.get("/get-all-manager-individual", validateCompanyOrManagerUser, userController.getAllManagerIndividual);
 route.post("/assign-course-to-sub-user", validateCompanyUserUser, userController.assignCourseToSubUser);
 route.post("/block-sub-user", validateCompanyUserUser, userController.blockSubUser);
