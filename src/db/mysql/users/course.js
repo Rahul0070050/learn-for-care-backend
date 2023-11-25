0247ff346014f30e6109edff21065df7d2f389f8
rahul <rahulor2000@gmail.com>
2023-11-25 13:45:02 +0530

    get course route updated

--- src/db/mysql/users/course.js
+++ src/db/mysql/users/course.js
@@ -59,9 +59,9 @@ export function getPurchasedCourseByUserId(id) {
       let getPurchasedCourseDataQuery = `
         SELECT purchased_course.id AS purchased_course_id, purchased_course.id AS id,
         1 AS from_purchased, Name, description, course_count,
-        course_id, category, validity,  
-        FROM purchased_course INNER JOIN course ON 
-        purchased_course.course_id = course.id
+        course_id, category, validity
+        FROM purchased_course 
+        INNER JOIN course ON purchased_course.course_id = course.id
         WHERE purchased_course.user_id = ? AND purchased_course.course_count >= ?;
       `;
 
