33a71da442cd127c04e1f810b415ee8304bd791f
rahul <rahulor2000@gmail.com>
2023-11-26 20:04:45 +0530

    get enrolled coure by is route updateed

---
+++ src/middlewares/isBlocked.js
@@ -0,0 +1,21 @@
+export function isUserActive(req, res, next) {
+  const { authorization } = req.headers;
+  const token = authorization?.split(" ")[1] || "";
+  validateIsUserActive(token)
+    .then((result) => {
+      next();
+    })
+    .catch((err) => {
+      res.status(401).json({
+        success: false,
+        errors: [
+          {
+            code: 401,
+            message: "please login",
+            error: err,
+          },
+        ],
+        errorType: "server",
+      });
+    });
+}
