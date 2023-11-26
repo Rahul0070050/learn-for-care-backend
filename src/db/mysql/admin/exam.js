2a7a87f9efdaeff4d0ec8512673d1659ff1b01ad
rahul <rahulor2000@gmail.com>
2023-11-26 22:51:25 +0530

    get enrolled coure by is route updateed

--- src/db/mysql/admin/exam.js
+++ src/db/mysql/admin/exam.js
@@ -30,7 +30,7 @@ export function getQuestionsForExamByCourseId(course_id) {
   return new Promise((resolve, reject) => {
     try {
       let getQuestionsQuery =
-        "SELECT * FROM exams WHERE course_id = ? LIMIT 10;";
+        "SELECT * FROM exams WHERE course_id = ?;";
       db.query(getQuestionsQuery, [course_id], (err, result) => {
         if (err) return reject(err?.message);
         else return resolve(result);
