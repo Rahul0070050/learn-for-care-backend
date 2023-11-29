934a462804998ab37a6f9f1b1ee9ac98dfc9f63c
rahul <rahulor2000@gmail.com>
2023-11-29 12:00:37 +0530

    some pages edites

--- src/controllers/user/bundleController.js
+++ src/controllers/user/bundleController.js
@@ -17,6 +17,7 @@ import {
 } from "../../db/mysql/users/courseBundle.js";
 import { getCourseByIdFromDb } from "../../db/mysql/users/course.js";
 import { getUser } from "../../utils/auth.js";
+import { getOnGoingCourseByIdFromDb } from "../../db/mysql/users/onGoingCourse.js";
 
 export const bundleController = {
   getBundleById: (req, res) => {
@@ -378,13 +379,97 @@ export const bundleController = {
       validateGetCourseReqData(req.body).then(result => {
         getOneCourseFromBundleCourse(result).then(result => {
           console.log(result);
-          res.status(200).json({
-            success: true,
-            data: {
-              code: 200,
-              message: "got courses",
-              response: result.course_id
-            },
+          getOnGoingCourseByIdFromDb(result,user.id).then(async (result) => {
+            let newResult = await result.map(async (course, i) => {
+              try {
+                console.log(course);
+                let resources = JSON.parse(course.resource);
+                let ppt = JSON.parse(course.ppt);
+
+                delete course.resource;
+                delete course.ppt;
+
+                course[`resourceCount`] = resources.length;
+                course[`pptCount`] = resources.length;
+
+                resources.forEach((item, i) => {
+                  console.log(item.file + ":" + item.type);
+                  course[`resource${i}-`] = item.file;
+                });
+
+                ppt.forEach((item, i) => {
+                  course[`ppt${i}-`] = item.file;
+                });
+
+                let images = [];
+                let resource = [];
+
+                for (let index = 0; index < ppt.length; index++) {
+                  let link = course[`ppt${index}-`];
+
+                  // let key = urlstring.pop();
+
+                  let url = await downloadFromS3(index, link);
+
+                  images.push(url.url);
+                }
+
+                for (let index = 0; index < resources.length; index++) {
+                  let link = course[`resource${index}-`];
+                  let url = await downloadFromS3(index, link);
+
+                  resource.push(url.url);
+                }
+
+                let intro_video = await downloadFromS3(
+                  course.id,
+                  course.intro_video
+                );
+
+                let thumbnail = await downloadFromS3(
+                  course.id,
+                  course.thumbnail
+                );
+
+                let video = await downloadFromS3(course.id, course.video);
+
+                course["intro_video"] = intro_video?.url;
+                course["thumbnail"] = thumbnail?.url;
+                course["video"] = video?.url;
+                course["ppt"] = images;
+                course["resource"] = resource;
+
+                return course;
+              } catch (error) {
+                console.log(error);
+              }
+            });
+
+            Promise.all(newResult)
+              .then((result) => {
+                res.status(200).json({
+                  success: true,
+                  data: {
+                    code: 200,
+                    message: `got one course`,
+                    response: result,
+                  },
+                });
+              })
+              .catch((err) => {
+                res.status(500).json({
+                  success: false,
+                  errors: [
+                    {
+                      code: 500,
+                      message:
+                        "some error occurred in the server try again after some times",
+                      error: err,
+                    },
+                  ],
+                  errorType: "server",
+                });
+              });
           });
         }).catch(err => {
           res.status(406).json({
