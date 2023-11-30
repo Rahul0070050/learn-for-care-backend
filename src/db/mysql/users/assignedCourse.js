df595e20745ec718f62b1e13dc3a97737ba48a85
rahul <rahulor2000@gmail.com>
2023-11-30 13:54:48 +0530

    get getcertificates route updated

--- src/db/mysql/users/assignedCourse.js
+++ src/db/mysql/users/assignedCourse.js
@@ -74,10 +74,7 @@ export function getAssignedCourseByIdFromManagerAssigned(id) {
   return new Promise((resolve, reject) => {
     try {
       let getCourseByIdQuery = `
-      SELECT course_assigned_manager.*,
-      FROM course_assigned_manager 
-      INNER JOIN course ON course.id = course_assigned_manager.course_id
-      WHERE course_assigned_manager.id = ?;`;
+      SELECT course_assigned_manager FROM course_assigned_manager WHERE course_assigned_manager.id = ?;`;
       db.query(getCourseByIdQuery, [id], (err, result) => {
         if (err) return reject(err.message);
         else return resolve(result);
