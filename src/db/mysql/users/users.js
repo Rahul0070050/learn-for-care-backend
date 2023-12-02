1fb7e6d8ae43958a5d99be84a7b0b2e0223a7e7f
rahul <rahulor2000@gmail.com>
2023-12-02 20:33:00 +0530

    company selfe assign course and bundle updated

--- src/db/mysql/users/users.js
+++ src/db/mysql/users/users.js
@@ -913,7 +913,7 @@ export function getCourseWiseManagerReportsFromDb(id) {
   return new Promise((resolve, reject) => {
     try {
       let getQuery = `
-        SELECT course.name AS course_name, COUNT(DISTINCT course_assigned_manager.owner) AS owner_count
+        SELECT course.name AS course_name, COUNT(DISTINCT course_assigned_manager.manager_id) AS owner_count
         FROM course_assigned_manager
         INNER JOIN course ON course.id = course_assigned_manager.course_id
         WHERE course_type = ? AND owner = ?
