ee66d43c2b8238cdb5dcb60e8811def6111adf0e
rahul <rahulor2000@gmail.com>
2023-12-03 16:21:42 +0530

    company matrix pages updated

--- src/db/mysql/users/invoice.js
+++ src/db/mysql/users/invoice.js
@@ -4,7 +4,7 @@ export function saveInvoiceToDb(data) {
   return new Promise((resolve, reject) => {
     try {
       const { user_id, image } = data;
-      let insertQuery = "INSERT INTO invoice (user_id, img) VALUES (?,?,?);";
+      let insertQuery = "INSERT INTO invoice (user_id, img) VALUES (?,?);";
       db.query(insertQuery, [user_id, image], (err, result) => {
         if (err) return reject(err?.message);
         else return resolve(result);
