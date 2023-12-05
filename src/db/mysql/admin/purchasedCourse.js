c5766f67a0f362cff5c7d9746f0692ad1f7b0801
rahul <rahulor2000@gmail.com>
2023-12-05 17:17:22 +0530

    coupon updated

--- src/db/mysql/admin/purchasedCourse.js
+++ src/db/mysql/admin/purchasedCourse.js
@@ -44,13 +44,17 @@ export function getAllPurchasedCourseFromDb() {
 export function getReportFromDb() {
   return new Promise((resolve, reject) => {
     try {
-      let getPurchasedCourseDataQuery = `SELECT
-      DATE(date) AS day,
-      SUM(amount) AS total_amount,
-      SUM(course_count) AS total_course_count
-      FROM purchased_course
-      GROUP BY day;
-    `;
+      let getPurchasedCourseDataQuery = `
+            SELECT
+                YEAR(date) AS year,
+                MONTH(date) AS month,
+                SUM(amount) AS total_amount,
+                SUM(course_count) AS total_course_count
+            FROM
+                purchased_course
+            GROUP BY
+                year, month;
+            `;
 
       db.query(getPurchasedCourseDataQuery, (err, result) => {
         if (err) {
@@ -64,17 +68,17 @@ export function getReportFromDb() {
     }
   });
 }
-export function getReportFromDbGroupByMonth() {
+export function getReportFromDbGroupByDay() {
   return new Promise((resolve, reject) => {
     try {
       let getPurchasedCourseDataQuery = `
       SELECT
-      MONTH(date) AS month,
+      YEAR(date) AS year,
       SUM(amount) AS total_amount,
       SUM(course_count) AS total_course_count
       FROM purchased_course
-      GROUP BY month;
-    `;
+      GROUP BY year;
+      `;
 
       db.query(getPurchasedCourseDataQuery, (err, result) => {
         if (err) {
@@ -91,22 +95,20 @@ export function getReportFromDbGroupByMonth() {
 export function getReportFromDbGroupByYear() {
   return new Promise((resolve, reject) => {
     try {
-      let getPurchasedCourseDataQuery = `SELECT
-      YEAR(date) AS year,
-      SUM(amount) AS total_amount,
-      SUM(course_count) AS total_course_count
+      let getPurchasedCourseCurrentDateQuery = `
+      SELECT *
       FROM purchased_course
-      GROUP BY year;
-    `;
+      WHERE DATE(date_column) = CURDATE();
+      `;
 
-      db.query(getPurchasedCourseDataQuery, async (err, result) => {
+      db.query(getPurchasedCourseCurrentDateQuery, async (err, result) => {
         if (err) {
           return reject(err?.message);
         } else {
-          let groupByYear = result;
-          let groupByMonth = await getReportFromDbGroupByMonth();
-          let groupByDay = await getReportFromDb();
-          return resolve({ groupByYear, groupByMonth, groupByDay });
+          let groupByYear = await getReportFromDb();
+          let groupByDay = await getReportFromDbGroupByDay();
+          let dailyReport = result
+          return resolve({ groupByYear, groupByDay, dailyReport });
         }
       });
     } catch (error) {
