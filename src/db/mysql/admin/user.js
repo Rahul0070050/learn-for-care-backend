e25101e39ecd583a170037fb7fbfbff8bcf1bddd
rahul <rahulor2000@gmail.com>
2023-12-06 02:37:47 +0530

    coupon updated

--- src/db/mysql/admin/user.js
+++ src/db/mysql/admin/user.js
@@ -104,6 +104,7 @@ export function getUserByIdFromDb(id) {
         if (err) {
           reject(err?.message);
         } else {
+          console.log(result[0]);
           let image = downloadFromS3("",result[0].profile_image || "")
           result[0].profile_image = image.url
           result[0].course_count = course[0]['COUNT(*)']
