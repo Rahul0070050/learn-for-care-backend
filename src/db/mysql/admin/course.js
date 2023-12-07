00c008f5c44081b47b4b01452434ada9233e1e2f
rahul <rahulor2000@gmail.com>
2023-12-08 05:25:14 +0530

    matrix route updated

--- src/db/mysql/admin/course.js
+++ src/db/mysql/admin/course.js
@@ -11,7 +11,7 @@ export function addNewCourse(courseData) {
         `INSERT INTO course(
           name,description,category,intro_video,thumbnail,video,ppt,resource,assessment,certificate,
           objective_define,What_you_will_learn,aims,who_should_attend,objectives_point,what_you_will_learn_point,
-          selling_price,RRP,course_type,duration,course_level,certificate,course_code
+          selling_price,RRP,course_type,duration,course_level,course_code
           ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);`;
       db.query(
         insertCourseQuery,
@@ -37,7 +37,6 @@ export function addNewCourse(courseData) {
           courseData.course_type,
           courseData.duration,
           courseData.course_level,
-          courseData.certificate,
           courseData.course_code,
         ],
         (err, result) => {
