20dbba25c5d8ae330ead8bfe30e54f4dc5503bc3
rahul <rahulor2000@gmail.com>
2023-12-08 13:22:46 +0530

    matrix route updated

--- src/controllers/admin/courseController.js
+++ src/controllers/admin/courseController.js
@@ -307,12 +307,15 @@ export const courseController = {
                   console.log(course.objectives_point);
                   console.log(course.what_you_will_learn_point);
                   console.log(course.who_should_attend);
+                  
+                  
                   course['aims'] = JSON.parse(course.aims || '[]')
                   course['objectives_point'] = JSON.parse(course.objectives_point || '[]')
                   course['what_you_will_learn_point'] = JSON.parse(course.what_you_will_learn_point || '[]')
                   course['who_should_attend'] = JSON.parse(course.who_should_attend || '[]')
 
                   console.log('=====================================================');
+                  
                   console.log(course.aims);
                   console.log(course.objectives_point);
                   console.log(course.what_you_will_learn_point);
