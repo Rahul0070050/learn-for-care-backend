04f711c75c336a8080713ba054abdacb07744c91
rahul <rahulor2000@gmail.com>
2023-12-09 20:10:00 +0530

    add course route updated

--- src/routes/user/onGoingCourse.js
+++ src/routes/user/onGoingCourse.js
@@ -7,7 +7,7 @@ import { validateUser } from "../../middlewares/userAuth.js";
 const route = Router();
 
 route.get("/get-on-going-course/:id",validateUser, onGoingCourseController.getOnGoingCourseById);
-route.get("/get-all-on-going-courses",validateUser, onGoingCourseController.getAllOnGoingCourseById);
+route.get("",validateUser, onGoingCourseController.getAllOnGoingCourseById);
 route.get("/get-all-on-going-courses",validateUser, onGoingCourseController.getCourseAttemptsById);
 
 export default route;
