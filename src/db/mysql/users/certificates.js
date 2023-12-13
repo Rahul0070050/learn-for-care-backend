8663dd521ddc53b58c71b5a32d5c49ce5360df2b
rahul <rahulor2000@gmail.com>
2023-12-13 23:18:06 +0530

    get certificates mysql get date query updated

--- src/db/mysql/users/certificates.js
+++ src/db/mysql/users/certificates.js
@@ -3,7 +3,7 @@ import { db } from "../../../conf/mysql.js";
 export function getAllCertificatesByUserId(id) {
     return new Promise((resolve, reject) => {
       try {
-        let getQuery = "SELECT * FROM certificate WHERE user_id = ?";
+        let getQuery = "SELECT *, DATE_FORMAT(date, '%d/%m/%Y') AS date FROM certificate WHERE user_id = ?";
         db.query(getQuery, [id], (err, result) => {
           if (err) return reject(err?.message);
           else return resolve(result);
