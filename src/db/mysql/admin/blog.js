74123f76e7befc5c5af7cb1388e07ea1466a77a0
rahul <rahulor2000@gmail.com>
2023-12-13 22:38:46 +0530

    list blogs mysql get date query updated

--- src/db/mysql/admin/blog.js
+++ src/db/mysql/admin/blog.js
@@ -37,7 +37,7 @@ export function getBlogByIdFromDb(id) {
 export function getAllBlogs() {
   return new Promise((resolve, reject) => {
     try {
-      let getAllBlogsQuery = "SELECT * FROM blogs WHERE state = 'published'";
+      let getAllBlogsQuery = "SELECT *, DATE_FORMAT(valid_till, '%d/%m/%Y') AS date FROM blogs WHERE state = 'published'";
       db.query(getAllBlogsQuery, (err, result) => {
         if (err) return reject(err?.message);
         else return resolve(result);
