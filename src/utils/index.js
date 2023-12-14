3c7a6fc407bda58d0c73a1ac20e12fc829de1e3e
rahul <rahulor2000@gmail.com>
2023-12-14 16:02:25 +0530

    course resourse updated

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,7 +4,7 @@ export function isValidFileExtensions(fileName, fileType) {
     video: ["mp4", , "mkv", "webm"],
     pptx: ["pptx"],
     pdf: ["pdf"],
-    resource: ["pdf", "mp4", "mkv", "webm", "txt", "docx", "jpg"],
+    resource: ["pdf", "mp4", "mkv", "webm", "txt", "docx", "jpg", "jpeg", "webp", "png"],
   };
   return (
     validFileExtensions[fileType].indexOf(fileName.split(".").pop() || "") > -1
