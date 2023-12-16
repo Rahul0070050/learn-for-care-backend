03e3a39759c639ac996c187e8913b976b3276241
rahul <rahulor2000@gmail.com>
2023-12-17 00:11:28 +0530

    forgot password email template updaed

--- src/db/mysql/users/users.js
+++ src/db/mysql/users/users.js
@@ -298,7 +298,7 @@ export function getAssignedBundleToManagerFromDb(userId) {
       SELECT course_bundle.price AS amount, course_bundle.name AS bundle_name, 
       course_bundle.id AS bundle_id, course_assigned_manager.count AS course_count, 
       course_assigned_manager.course_id AS course_id, course_assigned_manager.validity AS validity, 
-      course_assigned_manager.id AS id, true AS from_assigned_table 
+      course_assigned_manager.id AS id, true AS from_assigned_table, course_assigned_manager.owner AS owner
       FROM course_assigned_manager 
       INNER JOIN course_bundle ON course_bundle.id = course_assigned_manager.course_id 
       WHERE course_assigned_manager.manager_id = ?;`;
