3043f35a7d794053484c2c62578cee670024ed5f
rahul <rahulor2000@gmail.com>
2023-12-17 13:52:24 +0530

    course code added to the course-wise-individual report

--- src/db/mysql/users/users.js
+++ src/db/mysql/users/users.js
@@ -1130,7 +1130,7 @@ export function getCourseWiseIndividualFromManagerReportsFromDb(id) {
           let course = await getAllAssignedCourseByUserId(item.id);
           course.map((c) => {
             if (!course_names.find((item) => item?.course_name == c.name))
-              course_names.push({ course_name: c.name, count: 0 });
+              course_names.push({ course_name: c.name, count: 0 ,code: c.course_code});
           });
           item["course"] = course;
           return item;
