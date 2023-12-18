4cd901388d87db156121373fe6e012462a321b3b
rahul <rahulor2000@gmail.com>
2023-12-18 23:59:35 +0530

    debugging
    create certificate

--- src/certificate/courseCertificate.js
+++ src/certificate/courseCertificate.js
@@ -27,7 +27,7 @@ export async function saveCertificate({
         0,
         0,
         {
-          cover: [doc.page.width - 5, doc.page.height - 40],
+          cover: [doc.page.width - 0, doc.page.height - 80],
         }
       );
 
