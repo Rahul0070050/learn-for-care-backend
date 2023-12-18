544feda037edf782af7e116b3539664986177190
rahul <rahulor2000@gmail.com>
2023-12-19 00:32:37 +0530

    debugging
    create certificate

--- src/certificate/courseCertificate.js
+++ src/certificate/courseCertificate.js
@@ -13,7 +13,9 @@ export async function saveCertificate({
   return new Promise((resolve, reject) => {
     try {
       let description = "some text";
-      const doc = new PDFDocument();
+      const doc = new PDFDocument({
+        size: [612, 10334],
+      });
 
       let file_path = path.join(__dirname, "../", `/certificate/${filePath}`);
       let fileStream = fs.createWriteStream(file_path);
@@ -30,8 +32,8 @@ export async function saveCertificate({
           cover: [doc.page.width, doc.page.height - 150],
         }
       );
-      console.log('d w ',doc.page.width);
 
+      console.log('d h ', doc.page.height);
       doc.font("Helvetica-Bold");
 
       doc.fontSize(16);
