af8c558d7dd67ea3825d03110296b5fb262ff02c
rahul <rahulor2000@gmail.com>
2023-12-19 00:55:20 +0530

    debugging
    create certificate

--- src/certificate/courseCertificate.js
+++ src/certificate/courseCertificate.js
@@ -38,7 +38,7 @@ export async function saveCertificate({
 
       doc.fontSize(16);
       doc.fillColor("black");
-      doc.text(`${userName}`,0,500,{align: 'center'});
+      doc.text(`${userName}`,0,400,{align: 'center'});
 
       doc.end();
       fileStream.on("finish", () => {
