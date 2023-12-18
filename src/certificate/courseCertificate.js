d2249d56c1a149a4282cdcb26d7a35a2d852260f
rahul <rahulor2000@gmail.com>
2023-12-19 02:07:02 +0530

    debugging
    create certificate

--- src/certificate/courseCertificate.js
+++ src/certificate/courseCertificate.js
@@ -33,21 +33,25 @@ export async function saveCertificate({
         }
       );
 
+      let date = new Date().toLocaleDateString().split("/");
+      let newdate = date[1] + "/" + date[0] + "/" + date[2];
+
       const userNameWidth = doc.widthOfString(userName);
       const userNameHeight = doc.heightOfString(userName);
       const centerX = (doc.page.width - userNameWidth) / 2;
       const centerY = (doc.page.height - userNameHeight) / 2;
-      
+
       doc.font("Helvetica-Bold");
       doc.fontSize(16);
       doc.fillColor("black");
       doc.text(userName, centerX, centerY - 30);
 
-      doc.font('Helvetica');
-      
-      doc.text(courseName, 220,400)
-      doc.text(courseName, 220,450)
-      doc.text('some text for course description'.split(1,60), 220,500)
+      doc.font("Helvetica");
+
+      doc.text(courseName, 220, 470);
+      doc.text(newdate, 220, 420);
+      doc.text(sl, 220, 460);
+      doc.text("some text for course description".split(1, 60), 220, 600);
       doc.end();
       fileStream.on("finish", () => {
         resolve(file_path);
