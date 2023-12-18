ae0f9d31652c3cbab9471a1bc3bcb65ee23a5bbe
rahul <rahulor2000@gmail.com>
2023-12-18 09:06:53 +0530

    get on-going-bundles route updated
    debugging

--- src/controllers/user/bundleController.js
+++ src/controllers/user/bundleController.js
@@ -216,6 +216,7 @@ export const bundleController = {
                 console.log(startedResult);
                 console.log(startedResult.course_count);
                 console.log(JSON.parse(startedResult.course_count));
+                console.log(JSON.parse(startedResult.course_count).length);
                 let data = {
                   bundle_id: startedResult.id,
                   validity: startedResult.validity,
