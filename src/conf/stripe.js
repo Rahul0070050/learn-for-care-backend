a3a910b0c755f957258ab74ec217ed6d1e4bb7c2
rahul <rahulor2000@gmail.com>
2023-12-21 02:06:02 +0530

    some page updated
    cart

--- src/conf/stripe.js
+++ src/conf/stripe.js
@@ -18,7 +18,7 @@ export async function getStripeUrl(items = [], email) {
           product_data: {
             name: item.name,
           },
-          unit_amount: parseInt(item.amount / item.product_count) * 100,
+          unit_amount: Number((item.amount / item.product_count) * 100),
         },
         quantity: item.product_count,
       };
