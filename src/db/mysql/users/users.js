3371e287fedf104e61798614d01243958e96cccb
rahul <rahulor2000@gmail.com>
2023-12-21 02:54:47 +0530

    some page updated

--- src/db/mysql/users/users.js
+++ src/db/mysql/users/users.js
@@ -751,6 +751,42 @@ export function getAllManagerIndividualFromDb(id) {
   });
 }
 
+export function getAllManagerIndividualFromDbFromCompany(id) {
+  return new Promise((resolve, reject) => {
+    try {
+      let getQuery =
+        "SELECT city ,phone ,email ,first_name ,id ,joined ,last_name, block, type_of_account FROM users WHERE created_by = ? AND type_oc_account = ?";
+      db.query(getQuery, [id, "individual"], (err, result) => {
+        if (err) {
+          reject(err.message);
+        } else {
+          resolve(result);
+        }
+      });
+    } catch (error) {
+      reject(error?.message);
+    }
+  });
+}
+
+export function getAllManagerRealIndividualFromDb(id) {
+  return new Promise((resolve, reject) => {
+    try {
+      let getQuery =
+        "SELECT city ,phone ,email ,first_name ,id ,joined ,last_name, block, type_of_account FROM users WHERE created_by = ? AND type_of_account = ?";
+      db.query(getQuery, [id, "individual"], (err, result) => {
+        if (err) {
+          reject(err.message);
+        } else {
+          resolve(result);
+        }
+      });
+    } catch (error) {
+      reject(error?.message);
+    }
+  });
+}
+
 export function getAllIndividualUnderCompanyFromDb(id) {
   return new Promise(async (resolve, reject) => {
     let managers = await getAllMAnagers(id);
