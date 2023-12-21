90a94bcd5a7d02e14b8ca9b10afc63d3ec2b5577
rahul <rahulor2000@gmail.com>
2023-12-21 14:14:51 +0530

    added validation on coupon route in user side

--- src/routes/user/coupon.js
+++ src/routes/user/coupon.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
 
 import { couponController } from "../../controllers/user/couponController.js";
-import { validateAdmin } from "../../middlewares/adminAuth.js";
+import { validateUser } from "../../middlewares/userAuth.js";
 
 const route = Router();
 
-route.post("/apply-coupon",couponController.applyCoupon);
-route.post("/remove-coupon",couponController.removeCoupon);
-route.post("/get-applied-coupon",couponController.getAppliedCoupon);
-route.get("/get-offer-text",couponController.getOfferTexts);
+route.post("/apply-coupon", validateUser, couponController.applyCoupon);
+route.post("/remove-coupon", validateUser, couponController.removeCoupon);
+route.post(
+  "/get-applied-coupon",
+  validateUser,
+  couponController.getAppliedCoupon
+);
+route.get("/get-offer-text", validateUser, couponController.getOfferTexts);
 
 export default route;
