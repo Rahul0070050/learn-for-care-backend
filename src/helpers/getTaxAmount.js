d7280ac41234857819f64fcb61fceb3cd42e602b
rahul <rahulor2000@gmail.com>
2023-12-21 19:38:51 +0530

    invoice updated
    invoice line hight updated

--- src/helpers/getTaxAmount.js
+++ src/helpers/getTaxAmount.js
@@ -1,6 +1,6 @@
 export function calculateVATAmount(productPrice) {
   // Standard VAT rate for the tax year 2022/2023
-  const vatRate = 0.2; // 20%
+  const vatRate = 0.12; // 12%
 
   const vatAmount = parseFloat(productPrice * vatRate).toFixed(2);
   return vatAmount;
