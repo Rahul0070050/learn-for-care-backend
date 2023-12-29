5758b16bb9cf095f61f38a4e0eb36b0de247e4a5
rahul <rahulor2000@gmail.com>
2023-12-29 13:05:40 +0530

    node mailer updated

--- src/conf/nodeMailer.js
+++ src/conf/nodeMailer.js
@@ -4,7 +4,7 @@ export function mailer() {
   return nodemailer.createTransport({
     // service: "Outlook",
     service: "live.smtp.mailtrap.io",
-    secure: false,
+    // secure: false,
     port: 587,
     auth: {
       user: process.env.EMAIL_USER,
