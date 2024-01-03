89285a2d25198624030d03d9d12b2e83ce84de85
rahul <rahulor2000@gmail.com>
2024-01-03 14:26:14 +0530

    make certificate updated

--- src/certificate/courseCertificate.js
+++ src/certificate/courseCertificate.js
@@ -44,7 +44,7 @@ export async function saveCertificate({
 
       doc.font(path.join(__dirname, "../", "/font/Montserrat/static/Montserrat-Bold.ttf"));
       doc.fontSize(16);
-      doc.fillColor("#212A4F");
+      doc.fillColor("#fff");
       doc.text(userName, centerX, centerY - 30);
 
       doc.font("Helvetica");
